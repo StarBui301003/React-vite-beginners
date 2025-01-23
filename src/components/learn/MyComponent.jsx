c2aa1bf068c419a64c0c6d8e5c17b141503078ca
StarBui301003 <saobc3010@gmail.com>
2025-01-23 15:49:50 +0700

    vid 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,4 +1,13 @@
+import "./style.css";
 const MyComponent = () => {
-  return <div>Hello World</div>;
+  const hello = true;
+  return (
+    <>
+      <div>{hello} World</div>
+      <div className="child" style={{ borderRadius: "10px" }}>
+        child
+      </div>
+    </>
+  );
 };
 export default MyComponent;
