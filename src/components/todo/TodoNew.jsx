7ec3ce89d0a3fc9845d224c7f705acd7fdaea07f
StarBui301003 <saobc3010@gmail.com>
2025-01-23 22:15:05 +0700

    vid 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,11 +1,22 @@
 const TodoNew = (props) => {
   console.log(props);
   const { addNewTodo } = props;
-  addNewTodo("Sao");
+  //   addNewTodo("Sao");
+  const handleClick = () => {
+    alert("Call me");
+  };
+  const handleOnChange = (name) => {
+    console.log("handleOnChange", name);
+  };
   return (
     <div className="todo-new">
-      <input type="text" />
-      <button>Add</button>
+      <input
+        type="text"
+        onChange={(event) => handleOnChange(event.target.value)}
+      />
+      <button style={{ cursor: "pointer" }} onClick={handleClick}>
+        Add
+      </button>
     </div>
   );
 };
