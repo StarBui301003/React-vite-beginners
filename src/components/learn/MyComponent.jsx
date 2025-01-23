3fa53f414adc04f4d4a704ed603c0be5147cc64a
StarBui301003 <saobc3010@gmail.com>
2025-01-23 15:34:05 +0700

    vid 29

---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,4 @@
+const MyComponent = () => {
+  return <div>Hello World</div>;
+};
+export default MyComponent;
---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,7 @@
+const ThirdComponent = () => {
+  return <div>Third Component</div>;
+};
+const SecondComponent = () => {
+  return <div>SecondComponent</div>;
+};
+export { SecondComponent, ThirdComponent };
